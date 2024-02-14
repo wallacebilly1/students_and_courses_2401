0cf5e0b8a30824241a269ac5a1b38364f10c0f81
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:24:23 -0700

    Add test for accessing attribute data

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,6 +5,14 @@ require './lib/student'
 RSpec.describe Course do
   it 'exists' do
     course = Course.new("Calculus", 2)
+
     expect(course).to be_instance_of(Course)
+  end
+
+  it 'can access attribute data for a course object' do
+    course = Course.new("Calculus", 2)
+    
+    expect(course.subject).to eq "Calculus"
+    expect(course.capacity).to eq 2
   end
 end

3a386a7d8d3c6322a43ae04d4ab977536b3538c8
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:10:16 -0700

    Add test for attributes of student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,9 +2,16 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
+  it 'exists' do
     morgan = Student.new({name: "Morgan", age: 21})
     expect(morgan).to be_instance_of(Student)
+  end
+
+  it 'can access attribute data for a student object' do
+    morgan = Student.new({name: "Morgan", age: 21})
+
+    expect(morgan.name).to be "Morgan"
+    expect(morgan.age).to be 21
   end
 
   

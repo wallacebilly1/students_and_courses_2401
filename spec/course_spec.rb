f182e26622913210f985f56542a996df253547b6
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:22:14 -0700

    Create #initialize method for course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,4 +2,9 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
-
+RSpec.describe Course do
+  it 'exists' do
+    course = Course.new("Calculus", 2)
+    expect(course).to be_instance_of(Course)
+  end
+end

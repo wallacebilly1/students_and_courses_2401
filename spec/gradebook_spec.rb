d0c6ae9a33e4f21dc068a427c7b9c202084849b8
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:59:22 -0700

    Add start of test for #list_all_students method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,9 +5,11 @@ require './lib/course'
 
 RSpec.describe Gradebook do
   before(:each) do
-    @course = Course.new("Calculus", 2)
+    @calculus = Course.new("Calculus", 2)
     @student1 = Student.new({name: "Morgan", age: 21})
     @student2 = Student.new({name: "Jordan", age: 29})
+    @student3 = Student.new({name: "Tommy", age: 28})
+    @student4 = Student.new({name: "Veronica", age: 23})
     @dr_math_gradebook = Gradebook.new("Dr. Math")    
   end
 
@@ -24,9 +26,25 @@ RSpec.describe Gradebook do
   end
 
   it 'can add courses' do
-    new_course = @dr_math_gradebook.add_course(@course)
+    new_course = @dr_math_gradebook.add_course(@calculus)
 
-    expect(new_course).to eq [@course]
-    expect(@dr_math_gradebook.courses).to eq [@course]
+    expect(new_course).to eq [@calculus]
+    expect(@dr_math_gradebook.courses).to eq [@calculus]
+  end
+
+  it 'can return a list of all students' do
+    @calculus.enroll(@student1)
+    @calculus.enroll(@student2)
+
+    trigonometry = Course.new("Trigonometry", 4)
+    trigonometry.enroll(@student3)
+    trigonometry.enroll(@student4)
+
+    @dr_math_gradebook.add_course(@calculus)
+    @dr_math_gradebook.add_course(trigonometry)
+
+    student_list = @dr_math_gradebook.list_all_students
+
+    expect(student_list).to be_a Hash
   end
 end

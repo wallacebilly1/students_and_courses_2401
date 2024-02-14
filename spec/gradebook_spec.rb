6641e881921df310a084032d7c25c2ce6c2b772d
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:40:16 -0700

    Add test for accessing instructor attribute

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe Gradebook do
   before(:each) do
     @course = Course.new("Calculus", 2)
     @student1 = Student.new({name: "Morgan", age: 21})
-    @student2 = Student.new({name: "Jordan", age: 29})    
+    @student2 = Student.new({name: "Jordan", age: 29})
+    @dr_math_gradebook = Gradebook.new("Dr. Math")    
   end
 
   it 'exists' do
-    gradebook = Gradebook.new
-    expect(gradebook).to be_instance_of(Gradebook)
+    expect(@dr_math_gradebook).to be_instance_of(Gradebook)
+  end
+
+  it 'can access attribute data for a Gradebook object'
+    expect(@dr_math_gradebook.instructor).to eq "Dr. Math"
   end
 end

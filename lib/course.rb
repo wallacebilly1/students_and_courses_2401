1caedd073d48ccd1bdee0058a628a7ff87da8093
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:26:29 -0700

    Add empty students array to #initialize method

--- lib/course.rb
+++ lib/course.rb
@@ -1,9 +1,10 @@
 class Course
-  attr_reader :subject, :capacity
+  attr_reader :subject, :capacity, :students
 
   def initialize(subject, capacity)
     @subject = subject
     @capacity = capacity
+    @students = []
   end
  
 end

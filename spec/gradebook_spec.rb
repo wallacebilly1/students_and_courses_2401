89259d605236d54f56879240355d4d1edeea1915
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:37:20 -0700

    Add test for #initialize method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -3,3 +3,15 @@ require './lib/gradebook'
 require './lib/student'
 require './lib/course'
 
+RSpec.describe Gradebook do
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})    
+  end
+
+  it 'exists' do
+    gradebook = Gradebook.new
+    expect(gradebook).to be_instance_of(Gradebook)
+  end
+end

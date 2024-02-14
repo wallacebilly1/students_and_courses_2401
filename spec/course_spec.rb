685ebbf21d7f8e92eedf44ddde0503fa349bd892
Billy Wallace <wallace.billy1@gmail.com>
2024-02-14 09:32:35 -0700

    Add test for #enroll method and add before block to spec file

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,28 +3,34 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  it 'exists' do
-    course = Course.new("Calculus", 2)
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})    
+  end
 
-    expect(course).to be_instance_of(Course)
+  it 'exists' do
+    expect(@course).to be_instance_of(Course)
   end
 
   it 'can access attribute data for a course object' do
-    course = Course.new("Calculus", 2)
-    
-    expect(course.subject).to eq "Calculus"
-    expect(course.capacity).to eq 2
+    expect(@course.subject).to eq "Calculus"
+    expect(@course.capacity).to eq 2
   end
 
-  it 'has an empty array of students by default' do
-    course = Course.new("Calculus", 2)
-    
-    expect(course.students).to eq []
+  it 'has an empty array of students by default' do    
+    expect(@course.students).to eq []
   end
 
   it 'returns if the course is full' do
-    course = Course.new("Calculus", 2)
+    expect(@course.full?).to be false
+  end
 
-    expect(course.full?).to be false
+  it 'can #enroll students in courses' do
+    @course.enroll(student1)
+    expect(@course.students).to eq [@student1]
+
+    @course.enroll(student2)
+    expect(@course.students).to eq [@student1, @student2]
   end
 end
